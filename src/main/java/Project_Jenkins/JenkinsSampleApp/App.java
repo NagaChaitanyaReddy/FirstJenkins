8668e49c37a9d19ca32815ceaa534a7b4ed2c40c
Training_H2A.09.22 <Training_h2a.09.22@accenture.com>
2018-02-14 15:46:53 +0530

    This was MAVEN SAMPLE PROJECT

---
+++ src/main/java/Project_Jenkins/JenkinsSampleApp/App.java
@@ -0,0 +1,13 @@
+package Project_Jenkins.JenkinsSampleApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
